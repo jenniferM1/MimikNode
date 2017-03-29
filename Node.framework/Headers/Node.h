5bba2317c7bcf046a25c7e2585abfdbf26951e2c
mimikpods <mo.afrasiabi@mimik.com>
2017-03-29 12:05:58 -0700

    Initial Commit

---
+++ Node.framework/Headers/Node.h
@@ -0,0 +1,19 @@
+//
+//  Node.h
+//  Node
+//
+//  Created by Mo Afrasiabi on 2017-03-21.
+//  Copyright © 2017 mimik technology inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Node.
+FOUNDATION_EXPORT double NodeVersionNumber;
+
+//! Project version string for Node.
+FOUNDATION_EXPORT const unsigned char NodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Node/PublicHeader.h>
+
+#import <Node/NodeService.h>
---
+++ Node.framework/Headers/NodeService.h
@@ -0,0 +1,17 @@
+//
+//  NodeService.h
+//  Node
+//
+//  Created by Mo Afrasiabi on 2017-03-21.
+//  Copyright © 2017 mimik technology inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NodeService : NSObject
+
+- (instancetype)init;
+- (void)start;
+- (void)stop;
+
+@end
