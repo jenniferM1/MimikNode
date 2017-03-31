373ef52c76d51ed4fe71ee7d9e7d8e7cba20d233
mimikpods <mo.afrasiabi@mimik.com>
2017-03-31 15:00:10 -0700

    Updated Node Framework

--- Frameworks/Node.framework/Headers/NodeService.h
+++ Frameworks/Node.framework/Headers/NodeService.h
@@ -7,11 +7,28 @@
 //
 
 #import <Foundation/Foundation.h>
-
+/**
+ NodeService class instantiates MimikEdge, MimikDrive, MimikBeam services and manages their lifecycle.
+ To start these services simply instantiate NodeService and start the service when applicationDidBecomeActive and stop it when applicationWillResignActive.
+ */
 @interface NodeService : NSObject
 
+
+/**
+ NodeService initializer
+ */
 - (instancetype)init;
+
+
+/**
+ Starts MimikEdge, MimikDrive, MimikBeam services. This method should be called when the app activates (ex: applicationDidBecomeActive)
+ */
 - (void)start;
+
+
+/**
+ Stops MimikEdge, MimikDrive, MimikBeam services. This method should be called when the app is about to resign active (ex: applicationWillResignActive)
+ */
 - (void)stop;
 
 @end
